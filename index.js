2c6126b05c9747cf0dc0e97086c82bfb4e9ed775
6206021621155 <6206021621155@fitm.kmutnb.ac.th>
2021-02-04 15:39:40 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require("dontenv").config();
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/",(req, res) => {
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
